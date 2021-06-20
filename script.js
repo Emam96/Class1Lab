429ae8e692b5faed3b9ed2af43e3c1394e1aae0a
Emam96 <emamsh990@gmail.com>
2021-06-20 15:33:22 +0300

    added some alerts

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let userName = prompt("Enter your name ? ");
 console.log(userName);
 
+alert("hey "+userName);
+
 let lastName = prompt(
   "what is your last name? "
 );
@@ -9,8 +11,10 @@ console.log(lastName);
 let origin = prompt("From where are you?");
 console.log(origin);
 
+alert("wow! I love it there.");
+
 let age = prompt("How old are you?");
 console.log(age);
-
+alert("long live "+userName+"!");
 
 document.write("Hey "+userName +"-"+lastName+"! its nice to meet you! I like people from "+origin+" ,you are "+age+"! Mashallah!,")
