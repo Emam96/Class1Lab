def67d66112c268b18470828a3d37377f89faafd
Emam96 <emamsh990@gmail.com>
2021-06-20 15:19:50 +0300

    added the index and the script

---
+++ script.js
@@ -0,0 +1,16 @@
+let userName = prompt("Enter your name ? ");
+console.log(userName);
+
+let lastName = prompt(
+  "what is your last name? "
+);
+console.log(lastName);
+
+let origin = prompt("From where are you?");
+console.log(origin);
+
+let age = prompt("How old are you?");
+console.log(age);
+
+
+document.write("Hey "+userName +"-"+lastName+"! its nice to meet you! I like people from "+origin+" ,you are "+age+"! Mashallah!,")
